9c8209a6593a32e6528f61e6adabdf49e02a2bef
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-09 16:17:56 +0530

    feat: Update code to load and explore sample data

--- data_exploration.py
+++ data_exploration.py
@@ -1,24 +1,53 @@
 # importing modules/ libraries
 import pandas as pd
 
-# loading the data
+# loading and exploring the data
 aisles_df = pd.read_csv('Data/aisles.csv')
-print(aisles_df.head())
+print("\nFirst five rows of aisles.csv:\n", aisles_df.head(), sep="\n")
+print("\nDimensions of aisles.csv:\n", aisles_df.shape, sep="\n")
+print("\nNull values in aisles.csv:\n", (aisles_df.isnull()).sum(), sep="\n")
 
 departments_df = pd.read_csv('Data/departments.csv')
-print(departments_df.head())
+print("\nFirst five rows of departments.csv:\n", departments_df.head(), sep="\n")
+print("\nDimensions of departments.csv:\n", departments_df.shape, sep="\n")
+print("\nNull values in departments.csv:\n", (departments_df.isnull()).sum(), sep="\n")
 
-order_products__prior_df = pd.read_csv('Data/order_products__prior_sample.csv')
-print(order_products__prior_df.head())
+prior_sample_df = pd.read_csv('Data/order_products__prior_sample.csv')
+print("\nFirst five rows of order_products__prior_sample.csv:\n", 
+      prior_sample_df.head(), sep="\n")
+print("\nDimensions of order_products__prior_sample.csv:\n", 
+      prior_sample_df.shape, sep="\n")
+print("\nNull values in order_products__prior_sample.csv:\n", 
+      (prior_sample_df.isnull()).sum(), sep="\n")
 
-order_products__train_df = pd.read_csv('Data/order_products__train_sample.csv')
-print(order_products__train_df.head())
+train_sample_df = pd.read_csv('Data/order_products__train_sample.csv')
+print("\nFirst five rows of order_products__train_sample.csv:\n", 
+      train_sample_df.head(), sep="\n")
+print("\nDimensions of order_products__train_sample.csv:\n", 
+      train_sample_df.shape, sep="\n")
+print("\nNull values in order_products__train_sample.csv:\n", 
+      (train_sample_df.isnull()).sum(), sep="\n")
 
-orders_df = pd.read_csv('Data/orders_sample.csv')
-print(orders_df.head())
+orders_sample_df = pd.read_csv('Data/orders_sample.csv')
+print("\nFirst five rows of orders_sample.csv:\n", 
+      orders_sample_df.head(), sep="\n")
+print("\nDimensions of orders_sample.csv:\n", 
+      orders_sample_df.shape, sep="\n")
+print("\nNull values in orders_sample.csv:\n", 
+      (orders_sample_df.isnull()).sum(), sep="\n")
 
 products_df = pd.read_csv('Data/products.csv')
-print(products_df.head())
+print("\nFirst five rows of products.csv:\n", 
+      products_df.head(), sep="\n")
+print("\nDimensions of products.csv:\n", 
+      products_df.shape, sep="\n")
+print("\nNull values in products.csv:\n", 
+      (products_df.isnull()).sum(), sep="\n")
 
 sample_submission_df = pd.read_csv('Data/sample_submission.csv')
-print(sample_submission_df.head())
+print("\nFirst five rows of sample_submission.csv:\n", 
+      sample_submission_df.head(), sep="\n")
+print("\nDimensions of sample_submission.csv:\n", 
+      sample_submission_df.shape, sep="\n")
+print("\nNull values in sample_submission.csv:\n", 
+      (sample_submission_df.isnull()).sum(), sep="\n")
