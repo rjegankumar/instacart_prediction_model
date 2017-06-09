5e3a9ad00558547475e7b5674bb623cafc99643a
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-09 15:40:25 +0530

    fix: Update data explorations data sets to samples

--- data_exploration.py
+++ data_exploration.py
@@ -1,6 +1,5 @@
 # importing modules/ libraries
 import pandas as pd
-import random
 
 # loading the data
 aisles_df = pd.read_csv('Data/aisles.csv')
@@ -9,22 +8,13 @@ print(aisles_df.head())
 departments_df = pd.read_csv('Data/departments.csv')
 print(departments_df.head())
 
-#n = 32434489
-#s = round(0.1 * n)
-#skip = sorted(random.sample(range(1,n), n-s))
-order_products__prior_df = pd.read_csv('Data/order_products__prior.csv')
+order_products__prior_df = pd.read_csv('Data/order_products__prior_sample.csv')
 print(order_products__prior_df.head())
 
-#n = 1384617
-#s = round(0.1 * n)
-#skip = sorted(random.sample(range(1,n), n-s))
-order_products__train_df = pd.read_csv('Data/order_products__train.csv')
+order_products__train_df = pd.read_csv('Data/order_products__train_sample.csv')
 print(order_products__train_df.head())
 
-#n = 3421083
-#s = round(0.1 * n)
-#skip = sorted(random.sample(range(1,n), n-s))
-orders_df = pd.read_csv('Data/orders.csv')
+orders_df = pd.read_csv('Data/orders_sample.csv')
 print(orders_df.head())
 
 products_df = pd.read_csv('Data/products.csv')
