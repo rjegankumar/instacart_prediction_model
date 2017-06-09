6ee083f5b5a190f30f4916698c57c7ee1c2225fe
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-09 22:18:32 +0530

    fix: Change create sample code to ensure matching order ids data

--- create_sample.py
+++ create_sample.py
@@ -1,30 +1,28 @@
 # importing modules/ libraries
 import pandas as pd
 import random
+import numpy as np
 
 # create sample of order products train data
 n = 1384617
 s = round(0.1 * n)
 skip = sorted(random.sample(range(1,n), n-s))
 order_products__train_sample_df = pd.read_csv('Data/order_products__train.csv', 
-                                       skiprows = skip)
-order_products__train_sample_df.to_csv('Data/order_products__train_sample.csv', 
-                                       index = False)
+                                       index = 'order_Id', skiprows = skip)
+order_products__train_sample_df.to_csv('Data/order_products__train_sample.csv')
 
 # create sample of order products prior data
 n = 32434489
 s = round(0.1 * n)
 skip = sorted(random.sample(range(1,n), n-s))
 order_products__prior_sample_df = pd.read_csv('Data/order_products__prior.csv', 
-                                       skiprows = skip)
-order_products__prior_sample_df.to_csv('Data/order_products__prior_sample.csv', 
-                                       index = False)
+                                       index = 'order_Id', skiprows = skip)
+order_products__prior_sample_df.to_csv('Data/order_products__prior_sample.csv')
 
 # create sample of orders data
-n = 3421083
-s = round(0.1 * n)
-skip = sorted(random.sample(range(1,n), n-s))
-order_sample_df = pd.read_csv('Data/orders.csv', 
-                                       skiprows = skip)
-order_sample_df.to_csv('Data/orders_sample.csv', 
-                                       index = False)
+prior_unique_ids = np.array(order_products__prior_sample_df.index.unique())
+train_unique_ids = np.array(order_products__train_sample_df.index.unique())
+match_ids = np.concatenate((prior_unique_ids, train_unique_ids), axis = 0)
+order_sample_df = pd.read_csv('Data/orders.csv', index_col = 'order_id')
+order_sample_df = order_sample_df.loc[match_ids,:]
+order_sample_df.to_csv('Data/orders_sample.csv')
