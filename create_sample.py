e57ac5abecbd53f5ceaad56b38919eab38c4c2ac
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-10 23:29:48 +0530

    fix: Remove code to create sample for test orders

--- create_sample.py
+++ create_sample.py
@@ -14,11 +14,6 @@ s = round(131209 * 0.1)
 j = sorted(random.sample(list(orders_df[orders_df["eval_set"]=="train"].index), s))
 orders_df.loc[j,:].to_csv("Data/orders_train_sample.csv", index = False)
 
-# create a sample of test orders
-s = round(75000 * 0.1)
-k = sorted(random.sample(list(orders_df[orders_df["eval_set"]=="test"].index), s))
-orders_df.loc[k,:].to_csv("Data/orders_test_sample.csv", index = False)
-
 # create a sample of prior order products
 order_products_prior_df = pd.read_csv('Data/order_products__prior.csv', index_col = 'order_id')
 order_products_prior_df.loc[orders_df.loc[i,:]['order_id'],:].to_csv("Data/order_products_prior_sample.csv", index = False)
