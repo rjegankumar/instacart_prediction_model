fc86147b4579f7a6a1a0c66d3fb4ab99c47db790
Jegan Kumar Ramanathan <jeganram@umich.edu>
2017-06-19 19:52:25 +0530

    feat: Replace all NaNs with 0s in merged order prod data

--- data_preparation.py
+++ data_preparation.py
@@ -4,7 +4,9 @@ import numpy as np
 
 # loading data
 orders_prior_df = pd.read_csv('Data/orders_prior_sample.csv')
+print('\nFirst five rows of orders_prior_df:\n\n', orders_prior_df.head())
 orders_train_df = pd.read_csv('Data/orders_train_sample.csv')
+print('\nFirst five rows of orders_train_df:\n\n', orders_train_df.head())
 order_products_prior_df = pd.read_csv('Data/order_products_prior_sample.csv')
 order_products_train_df = pd.read_csv('Data/order_products_train_sample.csv')
 
@@ -31,17 +33,25 @@ def add_to_cart_orders(group):
 # merging prior product data with orders data
 grouped_pr = order_products_prior_df.groupby('order_id', as_index = False)
 grouped_data_pr = merging_prods_orders(grouped_pr)
-print('First five rows of grouped_data_pr:\n', grouped_data_pr.head())
+print('\nFirst five rows of grouped_data_pr:\n\n', grouped_data_pr.head())
 
 orders_prior_merged = pd.merge(orders_prior_df, grouped_data_pr, on='order_id')
-print('First five rows of orders_prior_merged:\n', orders_prior_merged.head())
+
+# replacing NaNs in days since prior order with 0
+orders_prior_merged[orders_prior_merged['days_since_prior_order'].isnull()]=0.0
+
+print('\nFirst five rows of orders_prior_merged:\n\n', orders_prior_merged.head())
 orders_prior_merged.to_csv('Data/orders_prior_merged.csv', index = False)
 
 # merging train product data with orders data
 grouped_tr = order_products_train_df.groupby('order_id', as_index = False)
 grouped_data_tr = merging_prods_orders(grouped_tr)
-print('First five rows of grouped_data_tr:\n', grouped_data_tr.head())
+print('\nFirst five rows of grouped_data_tr:\n\n', grouped_data_tr.head())
 
 orders_train_merged = pd.merge(orders_train_df, grouped_data_tr, on='order_id')
-print('First five rows of orders_train_merged:\n', orders_train_merged.head())
+
+# replacing NaNs in days since prior order with 0
+orders_train_merged[orders_train_merged['days_since_prior_order'].isnull()]=0.0
+
+print('\nFirst five rows of orders_train_merged:\n\n', orders_train_merged.head())
 orders_train_merged.to_csv('Data/orders_train_merged.csv', index = False)
